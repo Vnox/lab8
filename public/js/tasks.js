34da0f4229c247a9a5821ea725aeaa841b98df56
Shuangli Zhou <shz103@ucsd.edu>
2017-03-02 10:27:53 -0800

    task.js messed up

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,13 +1,4 @@
-exports.addTask = function(req, res){
-var taskName = req.body.taskName;
-  // This will print in your terminal when a POST is made
-  console.log(taskName); 
 
-  data.tasks.push(taskName); // Add to current data
-
-  // Lets send our task name back so our browser knows it worked out!
-  res.send(taskName);
-}// Your code goes here
 
 $(document).ready(function() {
 $('#taskForm').submit(function(e) {
@@ -21,8 +12,10 @@ $('#taskForm').submit(function(e) {
   });
 });
 
+
+
 function postCallback(res) {
   $('.taskList').append('' + res + ''); // Add to the list
-
  $('#addTask').val(''); // Clear form
 }
+
