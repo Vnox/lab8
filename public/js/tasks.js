abbe8fd5439040cde170a932e9aec1271c2933e3
Shuangli Zhou <shz103@ucsd.edu>
2017-03-02 10:14:38 -0800

    some more stuff

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,1 +1,28 @@
-// Your code goes here
+exports.addTask = function(req, res){
+var taskName = req.body.taskName;
+  // This will print in your terminal when a POST is made
+  console.log(taskName); 
+
+  data.tasks.push(taskName); // Add to current data
+
+  // Lets send our task name back so our browser knows it worked out!
+  res.send(taskName);
+}// Your code goes here
+
+$(document).ready(function() {
+$('#taskForm').submit(function(e) {
+    // Prevents default submit + reload (we only want the submit part)
+    e.preventDefault();
+
+    var taskName = $('#addTask').val();
+
+    // Send the POST request
+    $.post('addTask', { taskName: taskName }, postCallback);
+  });
+});
+
+function postCallback(res) {
+  $('.taskList').append('' + res + ''); // Add to the list
+
+ $('#addTask').val(''); // Clear form
+}
